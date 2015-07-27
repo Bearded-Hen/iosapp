badd305a943cb891c488d55c32372d30aec76414
Jamie Lynch <jamie@beardedhen.com>
2015-07-27 12:47:34 +0100

    close BLE connection when in background

--- Focus/FOCAppDelegate.m
+++ Focus/FOCAppDelegate.m
@@ -46,11 +46,7 @@ static NSString* kStorePath = @"focus.sqlite";
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     NSLog(@"Entering background!");
-    
-    // FIXME if playing program, maintain connection, otherwise close
-    
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [_focusDeviceManager closeConnection];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
